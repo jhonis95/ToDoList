909d65e8e0deef5c98ab1b2a11cabb2f9717c501
jonatan de oliveira <j.jhonis95@gmail.com>
2021-10-29 17:21:45 +0900

    adding new task function done

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
-const addTaskBtn= document.getElementsByClassName("addBtn");
 const taskTitle=document.getElementById("taskTitle");
 const form=document.getElementsByName('ToDo');
-addTaskBtn.onClick=addTask();
-function addTask(taskTitle,form){
-    // alert("Hello! I am an alert box!!");
-    form.forEach(Element => {//erro para usar o forEach
-        //some ideas for fix
-        //creat a array of elements and then use foreach
-        const input=document.createElemen("INPUT");
-        const label=document.createElement("LABEL");
 
-        input.type="checkbox";
-        label.value=form.value;
+function addTask(){
+    let li=document.createElement("li");
+    let ul=document.getElementById("list");
+    let inputValue=document.getElementById("newTask").value;
+    let text=document.createTextNode(inputValue);
 
-        taskTitle.appendChild(input);
-        taskTitle.appendChild(label)
-    });
-    alert("Hello! I am an alert box!!");
+    ul.appendChild(li);
+    li.appendChild(text);
+
+    document.getElementById("newTask").value = "";//to clean the label after submit
 };
