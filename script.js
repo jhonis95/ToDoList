3337d0a812d94ba94d5261beb0edab58f2bf2187
jonatan de oliveira <j.jhonis95@gmail.com>
2021-10-29 09:46:29 +0900

    making the button work and fixing few logic problems

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const addTaskBtn= document.getElementsByClassName("addBtn");
-const taskContainer=document.getElementsByClassName("taskList");
-addTaskBtn.onClick= function addTask(taskContainer){
-    const newTask=document.createElement("p");
-    taskContainer.apperchild(newTask);
-    newTask.textContent="new task"
+const taskTitle=document.getElementById("taskTitle");
+const form=document.getElementsByName('ToDo');
+addTaskBtn.onClick=addTask();
+function addTask(taskTitle,form){
+    // alert("Hello! I am an alert box!!");
+    form.forEach(Element => {//erro para usar o forEach
+        //some ideas for fix
+        //creat a array of elements and then use foreach
+        const input=document.createElemen("INPUT");
+        const label=document.createElement("LABEL");
+
+        input.type="checkbox";
+        label.value=form.value;
+
+        taskTitle.appendChild(input);
+        taskTitle.appendChild(label)
+    });
+    alert("Hello! I am an alert box!!");
 };
