70b740f57bd50aa97d8909fbc7de1bd6e107bdb4
jonatan de oliveira <j.jhonis95@gmail.com>
2021-10-31 01:22:54 +0900

    adding all the pages and start to make the logic to save the data

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const taskTitle=document.getElementById("taskTitle");
 const form=document.getElementsByName('ToDo');
 
 function addTask(){
+    //make item apere in the DOM
     let li=document.createElement("li");
     let ul=document.getElementById("list");
     let inputValue=document.getElementById("newTask").value;
@@ -9,6 +10,15 @@ function addTask(){
 
     ul.appendChild(li);
     li.appendChild(text);
+    //need to make
+    //save the data in a object
+    let task= {
+        check: false,
+        value: inputValue
+    };
+    let myToDo= [task];//array that have all the task
+    //need to make a function to put the objet in the array
+    console.log(myToDo);//chejing the object
 
     document.getElementById("newTask").value = "";//to clean the label after submit
 };
