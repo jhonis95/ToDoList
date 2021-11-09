26aeeba14e094ba5fbfe33393e8b1d8c13b17852
jonatan de oliveira <j.jhonis95@gmail.com>
2021-11-09 09:55:59 +0900

    creacte the arr that have all the task objs and start ti implement the AJAX

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 const taskTitle=document.getElementById("taskTitle");
 const form=document.getElementsByName('ToDo');
+const addBtn=document.getElementById('addBtn');
+
+addBtn.addEventListener('click',addTask)
+
+let myToDo= [];//array that have all the task
 
 function addTask(){
     //make item apere in the DOM
     let li=document.createElement("li");
     let ul=document.getElementById("list");
-    let inputValue=document.getElementById("newTask").value;
-    let text=document.createTextNode(inputValue);
+    let inputValue=document.getElementById("newTask").value;//variable that have the value of the input
+    let text=document.createTextNode(inputValue);//trasform the variable that have the value of the input in a usable string 
 
     ul.appendChild(li);
     li.appendChild(text);
-    //need to make
-    //save the data in a object
+
+    //creacting the task object
     let task= {
         check: false,
         value: inputValue
     };
-    let myToDo= [task];//array that have all the task
-    //need to make a function to put the objet in the array
-    console.log(myToDo);//chejing the object
+    myToDo.push(task)//pushing the task to array myToDo[]
+
+    // console.log(myToDo);//cheking the object
 
     document.getElementById("newTask").value = "";//to clean the label after submit
 };
+
+//XMLRequest AJAX
+const xmlContactPage=new XMLHttpRequest();
+xmlContactPage.open('GET','/contact.html',);
