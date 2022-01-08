1e3aaa88740b51886f446cabab126493ebd915b6
jonatan de oliveira <j.jhonis95@gmail.com>
2022-01-08 18:40:21 +0900

    side menu buttons setted and working fine

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let ex={
         value: "exemplo",
         delete: false
 }
+//add task menu
 let myToDo= [ex];//array that have all the task
 renderTask()
 function addTask(){
@@ -22,7 +23,35 @@ function addTask(){
     myToDo.push(task)//pushing the task to array myToDo[]
     
     renderTask();
+    rightShowSideMenu()
 };
+function checkHander(){
+    let checkBox = document.getElementsByClassName("taskCheckBox");
+    for(let i=0;i<checkBox.length;i++){
+        checkBox[i].addEventListener('change',(task)=>{//for each element in the list putting a event listener that do a function on change the checkbox
+            if(task.target.checked===true){ //task is the current element, .target is a reference to the object onto which the event was dispatched
+                myToDo[task.target.id].check=true//saving the tracked checkbox input to the current object
+            }else{
+                myToDo[task.target.id].check=false
+            }
+        })
+    }
+
+}
+function deleteHander(){
+    //adding the event to the delete button
+    let deleteBtn=document.getElementsByClassName("deleteBtn");//getting the HTML collection that have deleteBtn as class
+    for(let i=0;i<deleteBtn.length;i++){        
+        deleteBtn[i].addEventListener('click',(task)=>{//for each element of that collection add a event listener
+            myToDo[task.target.id].delete=true //setting the value of delete element to true in the array object
+            if(myToDo[task.target.id].delete===true){  // if that object have the elemente delete iquals true delete the object in the array
+                myToDo.splice(task.target.id,1)
+                renderTask()
+            }
+        })
+    }
+}
+//List menu
 function renderTask(){
     //make the container clean to render a new list
     let taskListContainer=document.getElementById("taskList")//div with the todo list
@@ -61,30 +90,24 @@ function renderTask(){
     deleteHander()//funtion to delete task on click button delete
     document.getElementById("newTask").value = "";//to clean the label after submit
 }
-function checkHander(){
-    let checkBox = document.getElementsByClassName("taskCheckBox");
-    for(let i=0;i<checkBox.length;i++){
-        checkBox[i].addEventListener('change',(task)=>{//for each element in the list putting a event listener that do a function on change the checkbox
-            if(task.target.checked===true){ //task is the current element, .target is a reference to the object onto which the event was dispatched
-                myToDo[task.target.id].check=true//saving the tracked checkbox input to the current object
-            }else{
-                myToDo[task.target.id].check=false
-            }
-        })
-    }
-
-}
-function deleteHander(){
-    //adding the event to the delete button
-    let deleteBtn=document.getElementsByClassName("deleteBtn");//getting the HTML collection that have deleteBtn as class
-    for(let i=0;i<deleteBtn.length;i++){        
-        deleteBtn[i].addEventListener('click',(task)=>{//for each element of that collection add a event listener
-            myToDo[task.target.id].delete=true //setting the value of delete element to true in the array object
-            if(myToDo[task.target.id].delete===true){  // if that object have the elemente delete iquals true delete the object in the array
-                myToDo.splice(task.target.id,1)
-                renderTask()
-            }
-        })
+//Side menu Button
+const rightSideMenuBtn=document.getElementById('rightSideMenuBtn')
+rightSideMenuBtn.addEventListener('click',rightShowSideMenu)
+function rightShowSideMenu(){
+    addMenu=document.getElementsByClassName("addTaskContainer")
+    if(addMenu[0].style.transform!=`translateX(${36}%)`){
+        addMenu[0].style.transform=`translateX(${36}%)`
+    }else{
+        addMenu[0].style.transform=`translateX(${126}%)`
     }
 }
-//Router 
+const leftSideMenuBtn=document.getElementById("leftSideMenuBtn")
+leftSideMenuBtn.addEventListener('click',leftShowSideMenu)
+function leftShowSideMenu(){
+    addMenu=document.getElementsByClassName("addLabelContainer")
+    if(addMenu[0].style.transform!=`translateX(${0}%)`){
+        addMenu[0].style.transform=`translateX(${0}%)`
+    }else{
+        addMenu[0].style.transform=`translateX(${-90}%)`
+    }
+}
