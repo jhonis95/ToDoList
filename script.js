284f80e8cd1e7ee769441712726c83dc039538ea
jonatan de oliveira <j.jhonis95@gmail.com>
2022-04-23 16:30:58 -0400

    make the edit label function functional

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ const addLabel=document.getElementById('addLabelBtn');
 const cancelAddLabelBtn=document.getElementById("cancelBtn")
 const confirmAddLabelBtn=document.getElementById("confirmBtn")
 
+
 addBtn.addEventListener('click',addTask)
 addLabel.addEventListener('click',openAddLabelModal)
 cancelAddLabelBtn.addEventListener('click',cancelAddNewLabel)
 confirmAddLabelBtn.addEventListener('click',confirmAddNewLabel)
 
 
-function label(labelName,task){//constructor
+function label(labelName){//constructor
   {
-    this.task=[task];
     this.labelName=labelName
   }
 }
@@ -22,27 +22,85 @@ function task(title,description,check,color,date){// constructor
   this.color= color;
   this.date= date;
 }
+//creacting the todo:
+let myToDo=[]//object that have all the labels
+myToDo["For Today"]=new label("For Today")
+myToDo["For Today"]["first task"]=new task("first task","for do today",false,"blue","22/03/2022")
 
-//add task menu
-let myToDo={
-  label: {
-    labelName:'For Today',
-    task:[
-      {
-        title:'first task',
-        description:'for do today',
-        check:false,
-        color:'blue',
-        date:'22/03/2022'
-      }
-    ]
-  }
-}//object that have all the labels
 
 //start render the list of task and labels
 renderTask()
 renderLabelList()
 
+//**********************************************/
+function openAddLabelModal(){
+    document.getElementById('newLabel').value=""
+    let modal = document.getElementById("addLabelModal");
+    modal.className="addLabelModal";
+}
+function confirmAddNewLabel(){
+    let newLabel=document.getElementById('newLabel').value;
+    myToDo[`${newLabel}`]=new label(newLabel);
+    cancelAddNewLabel();
+    renderLabelList();
+}
+function cancelAddNewLabel(){
+    let modal = document.getElementById("addLabelModal")
+    modal.className="addLabelModal hide"
+}this
+function renderLabelList(){
+    let labelListContainer= document.getElementById("labelListContainer")//Div container that have the label list
+    let oldUl=document.getElementById("labelList")//getting the old ul id 
+    let ul=document.createElement("ul")//creating new ul
+
+    labelListContainer.replaceChild(ul,oldUl) //update the label list replacing for a new ul 
+    ul.id="labelList"
+    
+    Object.keys(myToDo).forEach((label)=>{
+        let labelList=document.getElementById("labelList")
+        let li=document.createElement("li")
+        let editBtn=document.createElement("button")
+        let labelName=document.createElement("input")
+        let deleteBtn=document.createElement("button")
+
+        labelName.type="button"
+
+        labelName.value=myToDo[label].labelName
+        labelList.appendChild(li)
+        li.appendChild(deleteBtn)
+        li.appendChild(labelName)
+        li.appendChild(editBtn)
+
+        editBtn.className="editLabelBtn"
+        labelName.className="labelName"
+        deleteBtn.className="deleteLabel"
+
+        editBtn.addEventListener('click',editLabel)
+    })
+}
+function editLabel(){
+    let input=this.parentElement.children[1]
+    input.type="text"
+    input.className=`${input.className} inFocusToEdit`
+    let oldLabelName=input.value
+    console.log(Object.keys(myToDo))
+    input.addEventListener('keypress',(e)=>{
+        if(e.key==='Enter'){
+            Object.keys(myToDo).forEach((Label)=>{
+                if(oldLabelName===Label){
+                    myToDo[Label].labelName=input.value
+                    delete Object.assign(myToDo, {[input.value]: myToDo[Label] })[Label];
+                }
+            })
+            console.log(Object.keys(myToDo))
+            input.className=`labelName`
+            input.type="button"
+            renderLabelList()
+        }
+    })
+}
+// **********************************
+// need a revision all the functions
 function addTask(){
     let inputValue=document.getElementById("newTask").value;//variable that have the value of the input
     if(inputValue===""){//don't let the user put a empty input
@@ -92,10 +150,10 @@ function renderTask(){
     ul.id="list"//setting new ul to oldUl id to list in that way we dont have problem to reuse the render function
     
     //make list show in the DOM
-
-    Object.keys(myToDo.label).forEach((task)=>{//obj.key is a function that returns a array and forEach
-        if(myToDo.label.labelName===myToDo.label[task]){//dont print the name of the label
-            document.getElementById('labelName').innerText=myToDo.label[task]
+    let label=myToDo['For Today']
+    Object.keys(label).forEach((task)=>{//obj.key is a function that returns a array and forEach
+        if(label.labelName===label[task]){//dont print the name of the label
+            document.getElementById('labelName').innerText=label[task]
             return
         }
         let text=document.createTextNode(task)
@@ -110,7 +168,7 @@ function renderTask(){
         deleteBtn.className="deleteBtn"
         deleteBtn.id=`deleteBtn${task}`
 
-        if(myToDo.label[`${task}`].check===true){//render the input following the object value
+        if(label[`${task}`].check===true){//render the input following the object value
             input.checked=true 
         }else{
             input.checked=false
@@ -146,45 +204,4 @@ function leftShowSideMenu(){
     }else{
         addMenu[0].style.transform=`translateX(${-90}%)`
     }
-}
-//**********************************************/
-function openAddLabelModal(){
-    document.getElementById('newLabel').value=""
-    let modal = document.getElementById("addLabelModal");
-    modal.className="addLabelModal";
-}
-function confirmAddNewLabel(){
-    let newLabel=document.getElementById('newLabel').value;
-    myToDo[`${newLabel}`]=new label(newLabel);
-    cancelAddNewLabel();
-    renderLabelList();
-}
-function cancelAddNewLabel(){
-    let modal = document.getElementById("addLabelModal")
-    modal.className="addLabelModal hide"
-}
-function renderLabelList(){
-    let labelListContainer= document.getElementById("labelListContainer")//Div container that have the label list
-    let oldUl=document.getElementById("labelList")//getting the old ul id 
-    let ul=document.createElement("ul")//creating new ul
-
-    labelListContainer.replaceChild(ul,oldUl) //update the label list replacing for a new ul 
-    ul.id="labelList"
-    
-    Object.keys(myToDo).forEach((label)=>{
-        let labelList=document.getElementById("labelList")
-        let li=document.createElement("li")
-        let editBtn=document.createElement("button")
-        let labelName=document.createElement("h3")
-        let deleteBtn=document.createElement("button")
-
-        labelName.innerText=myToDo[label].labelName
-        labelList.appendChild(li)
-        li.appendChild(deleteBtn)
-        li.appendChild(labelName)
-        li.appendChild(editBtn)
-        editBtn.className="editLabelBtn"
-        labelName.className="labelName"
-        deleteBtn.className="deleteLabel"
-    })
 }
