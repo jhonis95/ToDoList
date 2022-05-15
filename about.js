6b305185e58aeee11979871fc3b93b2f22fdd3d8
jonatan de oliveira <j.jhonis95@gmail.com>
2022-05-14 21:29:29 -0400

    finishing the about page content (#2)

--- about.js
+++ about.js
@@ -1,5 +1,6 @@
 const devDescription=document.getElementById("devDescription")
 const devDescription2=document.getElementById("devDescription2")
+const aboutProject=document.getElementById("aboutProject")
 devDescription.textContent=`
 Estudar para me tornar programador me fez criar uma paixão por produtividade e otimização.
 Eu acredito que podemos sempre fazer as coisas melhores, não só no trabalho como também nas coisas do dia a dia,
@@ -7,3 +8,6 @@ acredito que por isso eu sou muito aberto para aprender e para ouvir críticas q
 `
 devDescription2.textContent+=`No Tempo que estudei fora do Brasil tive a oportunidade não só de aprender novas línguas, mas como também a conviver com pessoas de culturas diferentes que me fizeram ter uma mente mais aberta e um maior respeito ao próximo.
 Para o Futuro, me vejo trabalhando como desenvolvedor full stack ajudando aqueles que estão começando no desenvolvimento Web a criar grandes coisas.`
+
+aboutProject.textContent=`Esse projeto é para testar minhas habilidades em manipulação do DOM, 
+é um simples To Do List que permite o usuário separar suas tarefas em etiquetas para melhor organização dos seus afazeres. Essa aplicação web permite:`
