f82608a597cc7bdb3f436c796dc7b24700ffa82f
jonatan de oliveira <j.jhonis95@gmail.com>
2022-05-19 00:34:17 -0400

    start to create the contact (#5)

--- js/contact.js
+++ js/contact.js
@@ -1,27 +1,35 @@
-let contact=document.getElementsByClassName("social-media")
-const APIURL = 'https://api.github.com/users/'
-const githubUser = 'jhonis95'
-
+const contact=document.getElementsByClassName("social-media")
+// const APIURL = 'https://api.github.com/users/'
+// const githubUser = 'jhonis95'
 class socialMedia{
     constructor(icon,socialMedia,userName){
         this.icon=icon,
         this.socialMedia=socialMedia,
         this.userName=userName
     }
-}
-getUserGithub()
-async function getUserGithub(){
-    try{
-        const {data}=await axios(APIURL+githubUser)
-        console.log(data)
-        console.log("iniciou")
-    }catch(err){
-        if(err.response.status==404){
-            alert('No profile with this user name')
-        }
+    createSM=()=>{
+        let icon=document.createElement('div')
+        let socialMedia=document.createElement('h3')
+        let userName=document.createElement('h4')
+        let SM=document.createElement('li')
+        contact.appendChild(SM)
+
+        SM.appendChild(icon)
+        SM.appendChild(socialMedia)
+        SM.appendChild(userName)
+        
+        SM.className="socialMedia"
+
+        icon.style.backgroundImage=`url(${this.icon})`
+        icon.className="sm-icon"
+
+        socialMedia.className="sm-name"
+        socialMedia.innerText=this.socialMedia
+
+        userName.className="sm-user"
+        userName.innerText=this.userName
+        console.log("creating SM")
     }
 }
-let socialMedias=[]
-function getSocialMedia(data){
-    socialMedias= new socialMedia(data.avatar_url,"",data.name)
-}
+let contactME =new socialMedia("https://avatars.githubusercontent.com/jhonis95","GitHub","jhonis95")
+contactME.createSM
