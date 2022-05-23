6b9b910fe1901b43dbbf0d90a20c66e326e3a3a1
jonatan de oliveira <j.jhonis95@gmail.com>
2022-05-22 22:00:18 -0400

    Contact (#7)
    * start to create the contact
    
    * fixing the class social media

--- js/contact.js
+++ js/contact.js
@@ -1,35 +1,53 @@
-const contact=document.getElementsByClassName("social-media")
-// const APIURL = 'https://api.github.com/users/'
-// const githubUser = 'jhonis95'
+
+const contact=document.getElementById("social-media-list")
+
+
 class socialMedia{
-    constructor(icon,socialMedia,userName){
+    constructor(icon,socialMedia,description,link){
         this.icon=icon,
         this.socialMedia=socialMedia,
-        this.userName=userName
+        this.description=description
+        this.link=link
     }
-    createSM=()=>{
-        let icon=document.createElement('div')
+    renderSM(){
+        let icon=document.createElement('img')
         let socialMedia=document.createElement('h3')
-        let userName=document.createElement('h4')
+        let description=document.createElement('h4')
         let SM=document.createElement('li')
-        contact.appendChild(SM)
+        // let line=document.createElement('div')
+        contact.appendChild(SM) //erro here contact is a class no a id
 
         SM.appendChild(icon)
         SM.appendChild(socialMedia)
-        SM.appendChild(userName)
+        SM.appendChild(description)
         
         SM.className="socialMedia"
 
-        icon.style.backgroundImage=`url(${this.icon})`
+        icon.src=this.icon
+        icon.alt=`${this.socialMedia}Icon`
         icon.className="sm-icon"
 
         socialMedia.className="sm-name"
         socialMedia.innerText=this.socialMedia
 
-        userName.className="sm-user"
-        userName.innerText=this.userName
-        console.log("creating SM")
+        description.className="sm-description"
+        description.innerText=this.description
+
+        SM.addEventListener('click',this.goToLink)
     }
+    goToLink=()=>{
+        console.log("hi link is:"+this.link)
+        location.href=this.link//sende user to social media page
+    }
+
 }
-let contactME =new socialMedia("https://avatars.githubusercontent.com/jhonis95","GitHub","jhonis95")
-contactME.createSM
+let socialMediaList=[]
+socialMediaList.push(new socialMedia("https://cdn-icons-png.flaticon.com/512/733/733609.png","GitHub","here your can see more about what i had been doing ","https://github.com/jhonis95"))
+socialMediaList.push(new socialMedia("https://cdn-icons-png.flaticon.com/512/145/145807.png","LinkedIn","my profissional path and my conections","https://www.linkedin.com/in/jonatan-de-oliveira-4452bb209/"))
+socialMediaList.push(new socialMedia("https://cdn-icons.flaticon.com/png/512/3670/premium/3670151.png?token=exp=1653270960~hmac=2baabfc793ac4010b1d98606946d53d1","Twitter","here you can see more about me outside work","https://twitter.com/JJhonis95"))
+for(let task of socialMediaList){
+    task.renderSM()
+}
+
+
+
