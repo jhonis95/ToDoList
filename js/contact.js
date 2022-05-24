238eb4cfb6d553387fa3489544f417c3da4b79a7
jonatan de oliveira <j.jhonis95@gmail.com>
2022-05-23 22:14:44 -0400

    finishing new social media contact layout

--- js/contact.js
+++ js/contact.js
@@ -1,7 +1,6 @@
 
 const contact=document.getElementById("social-media-list")
 
-
 class socialMedia{
     constructor(icon,socialMedia,description,link){
         this.icon=icon,
@@ -13,19 +12,28 @@ class socialMedia{
         let icon=document.createElement('img')
         let socialMedia=document.createElement('h3')
         let description=document.createElement('h4')
+        let division=document.createElement('div')
+        let sign=document.createElement('h3')
         let SM=document.createElement('li')
         // let line=document.createElement('div')
         contact.appendChild(SM) //erro here contact is a class no a id
 
         SM.appendChild(icon)
+        SM.appendChild(division)
         SM.appendChild(socialMedia)
         SM.appendChild(description)
+        SM.appendChild(sign)
         
         SM.className="socialMedia"
 
         icon.src=this.icon
         icon.alt=`${this.socialMedia}Icon`
         icon.className="sm-icon"
+
+        division.className="sm-division"
+
+        sign.className="sm-sign"
+        sign.innerText=">"
 
         socialMedia.className="sm-name"
         socialMedia.innerText=this.socialMedia
@@ -44,7 +52,7 @@ class socialMedia{
 let socialMediaList=[]
 socialMediaList.push(new socialMedia("https://cdn-icons-png.flaticon.com/512/733/733609.png","GitHub","here your can see more about what i had been doing ","https://github.com/jhonis95"))
 socialMediaList.push(new socialMedia("https://cdn-icons-png.flaticon.com/512/145/145807.png","LinkedIn","my profissional path and my conections","https://www.linkedin.com/in/jonatan-de-oliveira-4452bb209/"))
-socialMediaList.push(new socialMedia("https://cdn-icons.flaticon.com/png/512/3670/premium/3670151.png?token=exp=1653270960~hmac=2baabfc793ac4010b1d98606946d53d1","Twitter","here you can see more about me outside work","https://twitter.com/JJhonis95"))
+socialMediaList.push(new socialMedia("https://cdn-icons-png.flaticon.com/512/145/145812.png","Twitter","here you can see more about me outside work","https://twitter.com/JJhonis95"))
 for(let task of socialMediaList){
     task.renderSM()
 }
