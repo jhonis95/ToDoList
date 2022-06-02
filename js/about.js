b2776e1e99c4bbad2fa152455123b6e9301be651
jonatan de oliveira <j.jhonis95@gmail.com>
2022-06-01 21:29:56 -0400

    fixings

--- js/about.js
+++ js/about.js
@@ -41,7 +41,7 @@ class card{
             this.render()
         }
     }
-    render=(evt)=>{
+    render=()=>{
         switch(true){
             case this.currentPosition===1:
                 aboutProject.textContent=this.aboutPj.content
