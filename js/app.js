f8d0066fcf1aaa55a8f059599c5a81608ae40f87
jonatan de oliveira <j.jhonis95@gmail.com>
2023-07-04 23:26:06 -0300

    add new Todo and open Modal is working fine

--- js/app.js
+++ js/app.js
@@ -1,21 +1,3 @@
-// export default function task(nome,description,date,check){
-    // function setParameters(newName,newDescription,newDate,newCheck){
-    //     return (
-    //         this.name=newName,
-    //         this.description=newDescription,
-    //         this.date=newDate,
-    //         this.check=newCheck
-    //     )
-    // }
-//     return{
-//         nome,
-//         description,
-//         date,
-//         check,
-//         setParameters
-//     }
-// }
-//******************** */
 import ToDo from "./todo.js"
 //app buttons
 const addTaskBtn=document.getElementById('addBtn');
@@ -27,6 +9,8 @@ const taskTitle=document.getElementById("newTask")//variable that have the value
 const taskDescription=document.getElementById("description")
 const taskDate=document.getElementById("date")
 const taskColor=document.getElementById("pickColor")
+//todo input
+const inputToDoName=document.getElementById("newLabel")
 
 const taskInput={
     taskTitle,
@@ -43,23 +27,74 @@ const appButtons={
 }
 
 class ToDoApp extends ToDo{
-    constructor(appButtons,taskInput){
+    constructor(appButtons,taskInput,toDoInput){
         super();
         this.listOfToDo=[]
         if(appButtons){
-            this.addTaskBtn=appButtons.addTaskBtn,
-            this.addLabelBtn=appButtons.addLabelBtn,
-            this.confirmAddLabelBtn=appButtons.confirmAddLabelBtn,
+            this.addTaskBtn=appButtons.addTaskBtn
+            this.addLabelBtn=appButtons.addLabelBtn
+            this.confirmAddLabelBtn=appButtons.confirmAddLabelBtn
             this.cancelAddLabelBtn=appButtons.cancelAddLabelBtn
         }
-        this.addTaskBtn=appButtons;
         if(taskInput){
             this.taskInputTitle=taskInput.taskNameInp
             this.taskInputDescription=taskInput.taskDescriptionInp
             this.taskInputColor=taskInput.taskColorInp
             this.taskInputDate=taskInput.taskDateInp
         }
-    }  
+        if(toDoInput){
+            this.toDoInput=toDoInput
+        }
+        this.toDoInput.addEventListener('change',(event)=>{
+            this.toDoName=event.target.value
+        })
+        this.addLabelBtn.addEventListener('click',this.openModal)
+        this.confirmAddLabelBtn.addEventListener('click',this.addToDo)
+    }
+
+    openModal=()=>{
+        document.getElementById("addLabelModal").className="addLabelModal"
+    }
+    addToDo=()=>{
+        this.listOfToDo.push(
+            new ToDo(this.toDoName)
+        )
+        this.renderToDoList()
+    }
+    renderToDoList(){
+        let labelListContainer= document.getElementById("labelListContainer")//Div container that have the label list
+        let oldUl=document.getElementById("labelList")//getting the old ul id 
+        let ul=document.createElement("ul")//creating new ul
+
+        labelListContainer.replaceChild(ul,oldUl) //update the label list replacing for a new ul 
+        ul.id="labelList"
+
+        this.listOfToDo.map((todo)=>{
+            let labelList=document.getElementById("labelList")
+            let li=document.createElement("li")
+            let editBtn=document.createElement("button")
+            let labelName=document.createElement("input")
+            let deleteBtn=document.createElement("button")
+
+            labelName.type="button"
+
+            labelName.value=todo.toDoName
+            labelList.appendChild(li)
+            li.appendChild(deleteBtn)
+            li.appendChild(labelName)
+            li.appendChild(editBtn)
+
+            editBtn.className="editLabelBtn"
+            labelName.className="labelName"
+            deleteBtn.className="deleteLabel"
+            //now who controu de app buttons is the class App
+            // editBtn.addEventListener('click',editLabel)
+            // deleteBtn.addEventListener('click',deleteLabel)
+            // labelName.addEventListener('click',setCurrentLabel)
+        })
+    }
 }
-const App= new ToDoApp(appButtons,taskInput)
+const App= new ToDoApp(appButtons,taskInput,inputToDoName)
+App.toDoName="For Today"
+App.addToDo()
 console.log(App)
--- js/todo.js
+++ js/todo.js
@@ -1,13 +1,16 @@
 class Task{
-    constructor() {
-        this.title,
-        this.description,
-        this.color,
-        this.date,
-        this.check=false
-    }
-    setTaskTitle=(title)=>{
+    constructor(title,description,color,date,check){
         this.title=title
+        this.description=description
+        this.color=color
+        this.date=date
+        this.check=check
+
+        this.tasckCheckBtn
+        this.taskDeleteBtn
+    }
+    setTaskTitle=(taskInput)=>{
+        this.title=taskInput
         console.log(this.title)
     }
     setTaskDescription=(taskInput)=>{
@@ -25,20 +28,57 @@ class Task{
     setTaskCheck=(taskInput)=>{
         this.check=taskInput
     }
+    set setTaskCheckBtn(button){
+        this.tasckCheckBtn=button
+    }
+    set setTaskDeleteBtn(button){
+        this.taskDeleteBtn=button
+    }
 }
 export default class ToDo extends Task{
-    constructor(){
+    constructor(toDoName){
         super();
-        this.toDoName=""
+        this.toDoName=toDoName
         this.toDoTaskList=[]
     }
     addTask=()=>{
-        this.toDoTaskList.push({
-            title : this.title,
-            description : this.description,
-            color: this.color,
-            date: this.date,
-            check: this.check
+        this.toDoTaskList.push(
+            new Task(this.title,this.description,this.color,this.date,this.check)
+        )
+        this.renderTaskList();
+    }
+    renderTaskList=()=>{
+        this.toDoTaskList.map((task)=>{
+            let li=document.createElement("li")
+            let taskName=document.createElement("h4")
+            let checkBtn=document.createElement("input")
+            let deleteBtn=document.createElement("button")
+            let taskDescription=document.createElement("output")
+            let taskDate=document.createElement("output")
+            let taskColor=document.createElement("button")
+
+            //setting the inputs type and class
+            taskName.className="taskNameOutput"
+            checkBtn.type="checkbox"
+            checkBtn.className="taskCheckBox"
+            deleteBtn.className="deleteBtn"
+            taskDescription.className="taskDescriptionOutput"
+            taskDate.className="taskDateOutput"
+            taskColor.className="taskColorOutput"
+
+            ul.appendChild(li)
+            li.appendChild(taskColor)
+            li.appendChild(checkBtn)
+            li.appendChild(taskName)
+            li.appendChild(taskDescription)
+            li.appendChild(taskDate)
+            li.appendChild(deleteBtn)
+
+            taskName.innerText=task.title;
+            taskDescription.value=task.description;
+            taskDate.value=task.date
+            taskColor.style.backgroundColor=task.color;
+            checkBtn.checked=task.check
         })
     }
 }
